91f11f608bf23851bab57bbbdc9ee5b8b4fe568c
edman90 <epedroza@purdue.edu>
2022-10-06 16:01:35 -0400

    Flask backend app route test

--- BackEnd/backend.py
+++ BackEnd/backend.py
@@ -1,7 +1,15 @@
-from flask import Flask
+import json
+from flask import Flask, jsonify
 from flask_restful import Resource, Api, reqparse, abort, marshal, fields
 
 # Initialize Flask
 app = Flask(__name__)
 api = Api(app)
+@app.route('/servo')
+def index():
 
+    return jsonify({'name': 'Natalie The National Park Worker',
+                    'email': 'natalie@outlook.com'})
+
+
+app.run(debug=True)
