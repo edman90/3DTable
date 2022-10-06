cfd7746bf298703fb2692e79365de591c493c4f5
edman90 <epedroza@purdue.edu>
2022-10-06 14:43:06 -0400

    Added leaflet map and servo button

--- UI/front-end/src/app/app.component.ts
+++ UI/front-end/src/app/app.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit} from '@angular/core';
+import * as L from 'leaflet';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   title = 'front-end';
+  // @ts-ignore
+  private map;
+
+  private initMap(): void {
+    this.map = L.map('map', {
+      center: [39.8282, -98.5795],
+      zoom: 3
+    });
+
+    const tiles = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      maxZoom: 18,
+      minZoom: 3,
+      attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    });
+
+    tiles.addTo(this.map);
+
+  }
+
+
+
+
+    constructor() { }
+
+    ngAfterViewInit(): void {
+      this.initMap();
+    }
 }
