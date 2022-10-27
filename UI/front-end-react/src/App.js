ffbc9af5ea3ac614e32af02474f482a24153d35c
RGross13 <gross92@purdue.edu>
2022-10-27 14:44:14 -0400

    Added some buttons and interactions

--- UI/front-end-react/src/App.js
+++ UI/front-end-react/src/App.js
@@ -4,16 +4,63 @@ import { MapContainer, TileLayer, Marker } from "react-leaflet";
 import '@bopen/leaflet-area-selection/dist/index.css';
 
 
-
+function locate() {
+    alert('You clicked locate!');
+}
+function reset() {
+    alert('You clicked reset!');
+}
+function send() {
+    alert('You clicked send to table!');
+}
 
 function App() {
 
-  return (<div>
-      <form>
+  return ( <body>
+      <div>
+          <header>
+              <a href="#" class="logo">Topographic Table</a>
+              <nav>
+                  <ul>
+                      <li><a href="#" style={{paddingRight: "70px"}}>Home</a></li>
+                      <li><a href="#">Sandbox</a></li>
+                  </ul>
+              </nav>
+          </header>
+          <div>
+          <div class="location">
+              <form style={{paddingTop: "40px"}}>
           <label>Enter an address:
               <input type="text" />
           </label>
-      </form>
+              </form>
+              <div>
+                <p>
+                    OR
+                </p>
+              </div>
+          </div>
+          <div class="location">
+              <form>
+                  <label>Enter a latitude:
+                      <input type="text" />
+                  </label>
+              </form>
+          </div>
+          <div class="location">
+              <form style={{paddingBottom: "30px"}}>
+                  <label>Enter a longitude:
+                      <input type="text" />
+                  </label>
+              </form>
+          </div>
+          </div>
+          <div class="buttons">
+          <button onClick={locate}>Locate</button>
+          <button onClick={reset}>Reset</button>
+          <button onClick={send}>Send to Table</button>
+          </div>
+          <div style={{clear: "both"}}>
       <MapContainer
           className="markercluster-map"
           center={[51.0, 19.0]}
@@ -26,7 +73,9 @@ function App() {
         />
 
       </MapContainer>
+          </div>
       </div>
+      </body>
   );
 }
 
